10b34a8645a25fd51d9f12226c7917a5b88f7b09
MoKe Programme <moke12g@gmail.com>
2018-01-13 23:38:22 +0100

    You can now calc whit 0,4

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		char rechenart = JOptionPane.showInputDialog("Möchtest du '+' '-' '*' oder '/' rechnen").charAt(0);
-		double wert1 = Double.parseDouble(JOptionPane.showInputDialog("Bitte gebe hier die erste Zahl ein"));
-		double wert2 = Double.parseDouble(JOptionPane.showInputDialog("Bitte gebe hier die zweite Zahl ein"));
+		String rawwert1 = JOptionPane.showInputDialog("Bitte gebe hier die erste Zahl ein");
+		rawwert1 = rawwert1.replace(',', '.');
+		double wert1 = Double.parseDouble(rawwert1);
+		String rawwert2 = JOptionPane.showInputDialog("Bitte gebe hier die zweite Zahl ein");
+		rawwert2 = rawwert2.replace(',', '.');
+		double wert2 = Double.parseDouble(rawwert2);
 		double ergebnis = 0;
 		if(rechenart == '+') ergebnis = wert1 + wert2;
 		if(rechenart == '-') ergebnis = wert1 - wert2;
 		if(rechenart == '*') ergebnis = wert1 * wert2;
 		if(rechenart == '/') ergebnis = wert1 / wert2;
-		JOptionPane.showMessageDialog(null, "Das Ergebnis der Rechnung ist " + ergebnis, "Wir haben über dein Ergebnis abgestimmt, es lautet", JOptionPane.OK_OPTION);
+		JOptionPane.showMessageDialog(null, "Das Ergebnis der Rechnung ist " + ergebnis, "Wir haben über dein Ergebnis abgestimmt", JOptionPane.OK_OPTION);
 	}
 
 }
