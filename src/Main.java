59395d1b1eb88dd06b3e4c9068653280d6bca92e
MoKe Programme <moke12g@gmail.com>
2018-01-13 23:33:03 +0100

    First Version Added much of Code
    Added much of Code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char rechenart = JOptionPane.showInputDialog("Möchtest du '+' '-' '*' oder '/' rechnen").charAt(0);
+		double wert1 = Double.parseDouble(JOptionPane.showInputDialog("Bitte gebe hier die erste Zahl ein"));
+		double wert2 = Double.parseDouble(JOptionPane.showInputDialog("Bitte gebe hier die zweite Zahl ein"));
+		double ergebnis = 0;
+		if(rechenart == '+') ergebnis = wert1 + wert2;
+		if(rechenart == '-') ergebnis = wert1 - wert2;
+		if(rechenart == '*') ergebnis = wert1 * wert2;
+		if(rechenart == '/') ergebnis = wert1 / wert2;
+		JOptionPane.showMessageDialog(null, "Das Ergebnis der Rechnung ist " + ergebnis, "Wir haben über dein Ergebnis abgestimmt, es lautet", JOptionPane.OK_OPTION);
 	}
 
 }
