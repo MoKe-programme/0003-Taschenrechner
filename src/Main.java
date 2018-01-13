e98804859764b23c7689cdb45904cac60461eb77
MoKe Programme <moke12g@gmail.com>
2018-01-13 23:55:55 +0100

    No wrong entered numbers anymore
    yay

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,49 @@
+import java.text.ParseException;
+
 import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
-		char rechenart = JOptionPane.showInputDialog("Möchtest du '+' '-' '*' oder '/' rechnen").charAt(0);
+		int i = 0;
+		char rechenart='ß';
+		while(i==0) {
+			try {
+		rechenart = JOptionPane.showInputDialog("Möchtest du '+' '-' '*' oder '/' rechnen").charAt(0);
+		if(rechenart=='+'||rechenart=='-'||rechenart=='*'||rechenart=='/')i++;
+			}
+			catch(Exception e) {}
+		}
+		i=0;
+		double wert1=0;
+		while(i==0) {
+			try {
 		String rawwert1 = JOptionPane.showInputDialog("Bitte gebe hier die erste Zahl ein");
 		rawwert1 = rawwert1.replace(',', '.');
-		double wert1 = Double.parseDouble(rawwert1);
+		if(rawwert1=="exit")i=4;
+		wert1 = Double.parseDouble(rawwert1);
+		i++;
+			}
+			catch(Exception e){}
+		}
+		if(i==1)i=0;
+		double wert2=0;
+		while(i==0) {
+		try {
 		String rawwert2 = JOptionPane.showInputDialog("Bitte gebe hier die zweite Zahl ein");
 		rawwert2 = rawwert2.replace(',', '.');
-		double wert2 = Double.parseDouble(rawwert2);
+		if(rawwert2=="exit")i=4;
+		wert2 = Double.parseDouble(rawwert2);
+		i++;
+		}
+		catch(Exception e) {}
+	}
 		double ergebnis = 0;
 		if(rechenart == '+') ergebnis = wert1 + wert2;
 		if(rechenart == '-') ergebnis = wert1 - wert2;
 		if(rechenart == '*') ergebnis = wert1 * wert2;
 		if(rechenart == '/') ergebnis = wert1 / wert2;
-		JOptionPane.showMessageDialog(null, "Das Ergebnis der Rechnung ist " + ergebnis, "Wir haben über dein Ergebnis abgestimmt", JOptionPane.OK_OPTION);
+		if(i==1)JOptionPane.showMessageDialog(null, "Das Ergebnis der Rechnung ist " + ergebnis, "Wir haben über dein Ergebnis abgestimmt", JOptionPane.OK_OPTION);
 	}
 
 }
